036902ec40d1c73f1bf508df7a620f79db5c7c76
unknown <almira.sagynbek@gmail.com>
2022-12-29 17:44:38 +0600

    second commit

--- js/events.js
+++ js/events.js
@@ -1,6 +1,36 @@
 const imgBlock = document.querySelector('.container')
+const firstImg = document.querySelector('.first-img')
+// const secondImg = document.querySelector('.second-img')
+// const thirdImg = document.querySelector('.third-img')
+// const images = new Array(
+//     './img/youme1.jpg', 
+//     './img/youme2.jpg', 
+//     './img/hands.jpg')
+
+// let i = 0
+// let j = images.length
+
+// secondImg.style.display = 'none'
+// thirdImg.style.display = 'none'
 
 imgBlock.addEventListener('click', () => {
     let audio = document.querySelector('audio');
         audio.play();
-})
+
+
+
+        // if (i!=(j-1)) {
+        //     i++
+        // }
+
+        // firstImg.src = images[i]
+}
+
+)
+
+// function changeImg() {
+//     firstImg.style.display = 'none'
+//     secondImg.style.display = 'block'
+// } 
+// setTimeout(changeImg, 20000)
+
